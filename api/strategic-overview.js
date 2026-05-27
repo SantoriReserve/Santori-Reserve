f6af3e20ac91c98d1b967382afb069d58aaf6125
any jazmin <anyjazmin@anys-Laptop.local>
2026-05-27 19:09:05 -0400

    fix strategic overview api

--- api/strategic-overview.js
+++ api/strategic-overview.js
@@ -8,19 +8,9 @@ export default async function handler(req, res) {
       body: JSON.stringify(req.body)
     });
 
+  
+       
     const data = await response.json();
-
-    export default async function handler(req, res) {
-      try {
-        const response = await fetch("https://hook.us2.make.com/278a8h6hk8mfurlmgin8k50to0mwe4h5", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(req.body)
-        });
-    
-        const data = await response.json();
     
         return res.status(200).json(data);
     
